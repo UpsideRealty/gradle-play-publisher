4e0d8449d58f7ca0add96cb99a202269fb6b97ce
a-mackay <a-mackay@users.noreply.github.com>
2024-10-31 11:04:00 +1100

    wip

--- play/plugin/src/test/kotlin/com/github/triplet/gradle/play/tasks/internal/PlayApiServiceTest.kt
+++ play/plugin/src/test/kotlin/com/github/triplet/gradle/play/tasks/internal/PlayApiServiceTest.kt
@@ -9,11 +9,11 @@ import org.mockito.Mockito.`when`
 import java.util.*
 
 class PlayApiServiceTest{
-    @Test
-    fun `creates correct PlayPublisher type based on configured credentials`() {
-        val serviceLoaderMock = mockStatic(ServiceLoader::class.java)
-        val factoryMock = mock(PlayPublisher.Factory::class.java)
-
-        `when`(serviceLoaderMock.load(PlayPublisher.Factory::class.java)).thenReturn(factoryMock)
-    }
+//    @Test
+//    fun `creates correct PlayPublisher type based on configured credentials`() {
+//        val serviceLoaderMock = mockStatic(ServiceLoader::class.java)
+//        val factoryMock = mock(PlayPublisher.Factory::class.java)
+//
+//        `when`(serviceLoaderMock.load(PlayPublisher.Factory::class.java)).thenReturn(factoryMock)
+//    }
 }
